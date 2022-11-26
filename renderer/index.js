c59f9c9256f41031ccdd28d8fe5fea3eae7a9213
tongyusheng <buaatys@163.com>
2022-11-26 20:10:21 +0800

    update doc

--- renderer/index.js
+++ renderer/index.js
@@ -1,7 +1,6 @@
 // 获取按钮元素
 var openVedioBtn = document.getElementById("openVedioBtn");
 var startRecordBtn = document.getElementById("startRecordBtn");
-var stopRecordBtn = document.getElementById("stopRecordBtn");
 var takePhotoBtn = document.getElementById("takePhotoBtn");
 
 // 绑定点击事件
@@ -26,19 +25,34 @@ window.onload = () => {
     });
   };
   // 录制视频
-  startRecordBtn.onclick = () => {};
-  // 结束录制
-  stopRecordBtn.onclick = () => {};
+  startRecordBtn.onclick = () => {
+    openVedioBtn.setAttribute("disabled", true);
+    startRecordBtn.setAttribute("disabled", true);
+    takePhotoBtn.setAttribute("disabled", true);
+    let photoWindow = window.open(
+      "html/record_window.html",
+      "_blank",
+      "width=300,height=300"
+    );
+    // 消息监听器-监听来自子窗口的消息
+    window.addEventListener("message", (msg) => {
+      if (JSON.stringify(msg.data) != null) {
+        if (photoWindow != null) {
+          photoWindow.close();
+          photoWindow = null;
+        }
+        openVedioBtn.removeAttribute("disabled");
+        startRecordBtn.removeAttribute("disabled");
+        takePhotoBtn.removeAttribute("disabled");
+      }
+    });
+  };
   // 拍照
   takePhotoBtn.onclick = () => {
     openVedioBtn.setAttribute("disabled", true);
     startRecordBtn.setAttribute("disabled", true);
     takePhotoBtn.setAttribute("disabled", true);
-    let photoWindow = window.open(
-      "html/photo_window.html",
-      "_blank",
-      "width=300,height=300"
-    );
+    let photoWindow = window.open("html/photo_window.html", "_blank");
     // 消息监听器-监听来自子窗口的消息
     window.addEventListener("message", (msg) => {
       if (JSON.stringify(msg.data) != null) {
