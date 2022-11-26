76e1f4d99be9e19f1daa4c4ad413d0ab2b6a97d0
tongyusheng <buaatys@163.com>
2022-11-27 02:27:11 +0800

    update doc

--- forge.config.js
+++ forge.config.js
@@ -3,7 +3,7 @@ module.exports = {
     appVersion: "1.0.0", //版本号
     name: "ICamera", // 应用名称
     appCopyright: "xiaotong(buaatys@163.com)", // 作者信息
-    icon: "./assets/logo.png",
+    icon: "assets/logo.ico",
   },
   rebuildConfig: {},
   makers: [
